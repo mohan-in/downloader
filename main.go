7820c2896f1c46e7917ca8ef912589f2b0276092
mohan <mohan@mohix.my.domain>
2015-11-07 05:24:20 +0530

    refactored

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ type resource struct {
 }
 
 type section struct {
+	id    int
 	start string
 	end   string
 	data  []byte
@@ -31,7 +32,7 @@ type section struct {
 func main() {
 
 	d := &resource{
-		url: "http://mirrors.mit.edu/pub/OpenBSD/5.8/i386/bsd.rd",
+		url: "http://mirrors.mit.edu/pub/OpenBSD/README",
 	}
 
 	req, err := http.NewRequest("HEAD", d.url, nil)
@@ -53,15 +54,18 @@ func main() {
 	var j int64 = 0
 	d.sections = make([]section, 5)
 	for i := 0; i < 5; i++ {
-		d.sections[i] = section{}
-		d.sections[i].data = d.data[j : j+d.sectionSize]
-		d.sections[i].start = strconv.FormatInt(j, 10)
+		d.sections[i] = section{
+			id:    i,
+			data:  d.data[j : j+d.sectionSize],
+			start: strconv.FormatInt(j, 10),
+		}
 		j += d.sectionSize
 		d.sections[i].end = strconv.FormatInt(j, 10)
 	}
 
 	for _, s := range d.sections {
-		go s.download(d.url, ch)
+		s := s
+		go download(&s, d.url, ch)
 	}
 
 	for i := 0; i < 5; i++ {
@@ -71,7 +75,7 @@ func main() {
 	ioutil.WriteFile("file", d.data, os.ModePerm)
 }
 
-func (s *section) download(url string, ch chan int) {
+func download(s *section, url string, ch chan int) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		fmt.Println(err)
@@ -91,20 +95,23 @@ func (s *section) download(url string, ch chan int) {
 	ticker := time.NewTicker(5 * time.Second)
 
 	go func() {
-		for {
-			tn, err := r.Read(s.data)
-			n = n + tn
-			if err == io.EOF {
-				ticker.Stop()
-				break
-			}
+		for _ = range ticker.C {
+			fmt.Println("Section: " + strconv.Itoa(s.id) + "; speed: " + strconv.Itoa(n/(1024*5)))
+			n = 0
 		}
 	}()
 
-	for _ = range ticker.C {
-		fmt.Println("speed: " + strconv.Itoa(n/(1024*5)))
-		n = 0
+	for {
+		tn, err := r.Read(s.data)
+		n = n + tn
+		if err == io.EOF {
+			ticker.Stop()
+			break
+		}
 	}
+
+	fmt.Println("Section " + strconv.Itoa(s.id) + " completed")
+	fmt.Println("Section " + strconv.Itoa(s.id) + " data " + string(s.data))
 
 	ch <- 0
 }
