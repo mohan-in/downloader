7558a4bffcd4ae164e8a928261b199ce6f493ff1
gocode <mail2mohanram@gmail.com>
2015-11-12 03:59:58 +0530

    refactored

---
+++ download.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+var (
+	client http.Client
+)
+
+type Resource struct {
+	url         string
+	data        []byte
+	size        int64
+	sectionSize int64
+	sections    []Section
+	fileName    string
+}
+
+type Section struct {
+	id    int
+	start int64
+	end   int64
+	data  []byte
+	speed chan int64
+}
+
+func (res *Resource) Download() {
+	req, err := http.NewRequest("HEAD", res.url, nil)
+	if err != nil {
+		logger.Println(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		logger.Println(err)
+	}
+
+	res.size = resp.ContentLength
+	res.sectionSize = res.size / 5
+	res.data = make([]byte, res.size)
+
+	var j int64 = 0
+	res.sections = make([]Section, 5)
+	for i := 0; i < 5; i++ {
+		res.sections[i] = Section{
+			id:    i,
+			data:  res.data[j : j+res.sectionSize],
+			start: j,
+			speed: make(chan int64),
+		}
+		j += res.sectionSize
+		res.sections[i].end = j - 1
+	}
+}
+
+func (s *Section) Download(url string, ch chan int) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		logger.Println(err)
+	}
+
+	req.Header.Add("Range", fmt.Sprintf("bytes=%d-%d", s.start, s.end))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		logger.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	var bufSize, sectionSize int64
+
+	ticker := time.NewTicker(5 * time.Second)
+	go func() {
+		for _ = range ticker.C {
+			s.speed <- bufSize / (1024 * 5)
+			bufSize = 0
+		}
+	}()
+
+	buf := make([]byte, 128*1024)
+	for {
+		n, err := resp.Body.Read(buf)
+
+		copy(s.data[sectionSize:], buf[0:n])
+		sectionSize += int64(n)
+
+		bufSize = bufSize + int64(n)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	logger.Printf("Section %d completed", s.id)
+
+	ticker.Stop()
+	close(s.speed)
+	ch <- s.id
+}
--- main.go
+++ main.go
@@ -2,85 +2,40 @@ package main
 
 import (
 	"flag"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
-	"time"
 )
 
 var (
-	client http.Client
 	logger *log.Logger
 	url    string
+	daemon bool
 )
-
-type resource struct {
-	url         string
-	data        []byte
-	size        int64
-	sectionSize int64
-	sections    []section
-	fileName    string
-}
-
-type section struct {
-	id    int
-	start int64
-	end   int64
-	data  []byte
-}
 
 func init() {
 	client = http.Client{}
 	logger = log.New(os.Stdout, "downloader: ", log.Lshortfile)
+	flag.BoolVar(&daemon, "daemon", false, "launch as daemon")
 	flag.StringVar(&url, "file", "", "the file to download")
 }
 
 func main() {
 	flag.Parse()
 
-	res := &resource{
+	res := &Resource{
 		url: url,
 	}
 
-	res.download()
-}
-
-func (res *resource) download() {
-	req, err := http.NewRequest("HEAD", res.url, nil)
-	if err != nil {
-		logger.Println(err)
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		logger.Println(err)
-	}
-
-	res.size = resp.ContentLength
-	res.sectionSize = res.size / 5
-	res.data = make([]byte, res.size)
+	res.Download()
 
 	ch := make(chan int)
 
-	var j int64 = 0
-	res.sections = make([]section, 5)
-	for i := 0; i < 5; i++ {
-		res.sections[i] = section{
-			id:    i,
-			data:  res.data[j : j+res.sectionSize],
-			start: j,
-		}
-		j += res.sectionSize
-		res.sections[i].end = j - 1
-	}
-
 	for _, s := range res.sections {
 		s := s
-		go s.download(res.url, ch)
+		go s.Download(res.url, ch)
+		go listen(&s)
 	}
 
 	for i := 0; i < 5; i++ {
@@ -90,47 +45,8 @@ func (res *resource) download() {
 	ioutil.WriteFile("file", res.data, os.ModePerm)
 }
 
-func (s *section) download(url string, ch chan int) {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		logger.Println(err)
+func listen(s *Section) {
+	for spd := range s.speed {
+		logger.Printf("Section: %d; speed: %d KB/s", s.id, spd)
 	}
-
-	req.Header.Add("Range", "bytes="+strconv.FormatInt(s.start, 10)+"-"+strconv.FormatInt(s.end, 10))
-
-	resp, err := client.Do(req)
-	if err != nil {
-		logger.Println(err)
-	}
-
-	defer resp.Body.Close()
-
-	var n, size int64
-
-	ticker := time.NewTicker(5 * time.Second)
-	go func() {
-		for _ = range ticker.C {
-			logger.Println("Section: " + strconv.Itoa(s.id) + "; speed: " + strconv.FormatInt(n/(1024*5), 10) + "KB/s")
-			n = 0
-		}
-	}()
-
-	buf := make([]byte, 128*1024)
-	for {
-		tn, err := resp.Body.Read(buf)
-
-		copy(s.data[size:], buf[0:tn])
-		size += int64(tn)
-
-		n = n + int64(tn)
-
-		if err == io.EOF {
-			ticker.Stop()
-			break
-		}
-	}
-
-	logger.Println("Section " + strconv.Itoa(s.id) + " completed")
-
-	ch <- 0
 }
